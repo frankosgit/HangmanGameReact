9ff1bb247af5e316e988a3f98d2b15310766fa1d
frankosgit <132131624+frankosgit@users.noreply.github.com>
2024-02-23 10:22:23 +0100

    updates to text logic

--- hangmanreact/src/App.tsx
+++ hangmanreact/src/App.tsx
@@ -1,11 +1,13 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 import './App.css'
 import Game from './components/Game'
 import { Player } from './models/playerModels'
+import { avatarText } from './models/avatartext'
 
 function App() {
-  const [playerOne, setPlayerOne] = useState(new Player("", 5, true))
+  const [playerOne, setPlayerOne] = useState(new Player("", 5, false))
   const [playerTwo, setPlayerTwo] = useState(new Player("", 5, false))
+  const [textDisplay, setTextDisplay] = useState(new avatarText(false, false, false, false))
 
   const handlePlayerOneChange = (e: React.ChangeEvent<HTMLInputElement>) => {
     setPlayerOne({ ...playerOne, [e.target.name]: e.target.value })
@@ -17,53 +19,92 @@ function App() {
 
   const playerOneSubmit = (e: React.MouseEvent<HTMLButtonElement, MouseEvent>) => {
     e.preventDefault()
-    setPlayerOne({...playerOne, active: false})
-    setPlayerTwo({...playerTwo, active: true})
+    setPlayerOne({ ...playerOne, active: false })
+    setPlayerTwo({ ...playerTwo, active: true })
   }
+
+  const playerTwoSubmit = (e: React.MouseEvent<HTMLButtonElement, MouseEvent>) => {
+    e.preventDefault()
+    setPlayerOne({ ...playerOne, active: false })
+    setPlayerTwo({ ...playerTwo, active: false })
+  }
+
+  useEffect(() => {
+    setTimeout(() => {
+      setTextDisplay({ ...textDisplay, firstText: true })
+    }, 1000);
+    setTimeout(() => {
+      setTextDisplay({ ...textDisplay, secondText: true })
+    }, 3000);
+    setTimeout(() => {
+      setTextDisplay({ ...textDisplay, thirdText: true })
+    }, 5000);
+    setTimeout(() => {
+      setTextDisplay({ ...textDisplay, foruthText: true })
+      setPlayerOne({...playerOne, active: true})
+    }, 7000);
+  }, [])
+
+
 
   return (
     <>
       <div className='w-full'>
         <h1 className='mb-12'>HANGMAN GAME</h1>
-        <div className="avatar indicator">
-          <span className="indicator-item badge badge-secondary">typing…</span>
-          <div className="w-20 h-20 rounded-lg">
-            <img alt="Tailwind CSS examples" src="https://daisyui.com/images/stock/photo-1534528741775-53994a69daeb.jpg" />
-          </div>
-        </div>
-        <div className="chat chat-start">
-          <div className="chat-image avatar">
-            <div className="w-10 rounded-full">
-              <img alt="Tailwind CSS chat bubble component" src="https://daisyui.com/images/stock/photo-1534528741775-53994a69daeb.jpg" />
+        {textDisplay.firstText &&
+          <div className="avatar indicator mb-12">
+            <span className="indicator-item badge badge-secondary">typing…</span>
+            <div className="w-20 h-20 rounded-lg">
+              <img alt="Tailwind CSS examples" src="https://daisyui.com/images/stock/photo-1534528741775-53994a69daeb.jpg" />
             </div>
           </div>
-          <div className="chat-bubble">It was said that you would, destroy the Sith, not join them.</div>
-        </div>
-        <div className="chat chat-start">
-          <div className="chat-image avatar">
-            <div className="w-10 rounded-full">
-              <img alt="Tailwind CSS chat bubble component" src="https://daisyui.com/images/stock/photo-1534528741775-53994a69daeb.jpg" />
+        }
+        {textDisplay.secondText &&
+          <div className="chat chat-start mb-12">
+            <div className="chat-image avatar">
+              <div className="w-10 rounded-full">
+                <img alt="Tailwind CSS chat bubble component" src="https://daisyui.com/images/stock/photo-1534528741775-53994a69daeb.jpg" />
+              </div>
             </div>
+            <div className="chat-bubble">Lets play a game of hangman</div>
           </div>
-          <div className="chat-bubble">It was you who would bring balance to the Force</div>
-        </div>
+        }
+        {textDisplay.thirdText &&
+          <div className="chat chat-start mb-12">
+            <div className="chat-image avatar">
+              <div className="w-10 rounded-full">
+                <img alt="Tailwind CSS chat bubble component" src="https://daisyui.com/images/stock/photo-1534528741775-53994a69daeb.jpg" />
+              </div>
+            </div>
+            <div className="chat-bubble">Enter your name and we can go further</div>
+          </div>
+        }
+        {textDisplay.foruthText &&
+          <div className="chat chat-start mb-12">
+            <div className="chat-image avatar">
+              <div className="w-10 rounded-full">
+                <img alt="Tailwind CSS chat bubble component" src="https://daisyui.com/images/stock/photo-1534528741775-53994a69daeb.jpg" />
+              </div>
+            </div>
+            <div className="chat-bubble">Good luck!</div>
+          </div>
+        }
+        {
+        <>
         <div className="chat chat-start mb-12">
-          <div className="chat-image avatar">
-            <div className="w-10 rounded-full">
-              <img alt="Tailwind CSS chat bubble component" src="https://daisyui.com/images/stock/photo-1534528741775-53994a69daeb.jpg" />
-            </div>
-          </div>
-          <div className="chat-bubble">Not leave it in Darkness</div>
-          <Game />
-        </div>
-      </div>
-      {playerOne.active && <input name='name' type="text" placeholder="Type here" className="input input-bordered input-primary w-full max-w-xs" onChange={handlePlayerOneChange} />}
-      {playerTwo.active && <input name='name' type="text" placeholder="Type here" className="input input-bordered input-secondary w-full max-w-xs" onChange={handlePlayerTwoChange} />}
-      {playerOne.active && <button className="btn btn-outline btn-primary" onClick={playerOneSubmit}>PlayerOneSubmit</button>}
-      {playerTwo.active && <button className="btn btn-outline btn-secondary">PlayerTwoSubmit</button>}
+  
 
-      <h4>player one is called {playerOne.name}</h4>
-      <h4>player two is called{playerTwo.name}</h4>
+
+        </div>
+        </>
+        }
+      </div>
+
+      {playerOne.active && <input name='name' type="text" placeholder="Type here" className="input input-bordered input-primary w-full max-w-xs mb-12" onChange={handlePlayerOneChange} />}
+      {playerTwo.active && <input name='name' type="text" placeholder="Type here" className="input input-bordered input-secondary w-full max-w-xs mb-12" onChange={handlePlayerTwoChange} />}
+      {playerOne.active && <button className="btn btn-outline btn-primary" onClick={playerOneSubmit}>PlayerOneSubmit</button>}
+      {playerTwo.active && <button className="btn btn-outline btn-secondary" onSubmit={playerTwoSubmit}>PlayerTwoSubmit</button>}
+      <Game />
 
     </>
   )
---
+++ hangmanreact/src/models/avatartext.ts
@@ -0,0 +1,8 @@
+export class avatarText {
+    constructor (
+        public firstText: boolean,
+        public secondText: boolean,
+        public thirdText: boolean,
+        public foruthText: boolean
+    ) {}
+};

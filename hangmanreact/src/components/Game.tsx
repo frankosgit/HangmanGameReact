afd56eb9fd607a460a8880dac790a4a6aecb53c4
Jonnie Möller <scarabeo85@msn.com>
2024-02-23 09:58:20 +0100

    yee

---
+++ hangmanreact/src/components/Game.tsx
@@ -0,0 +1,74 @@
+import { useEffect, useState } from "react";
+
+const Game = () => {
+  const [randomWord, setRandomWord] = useState("");
+
+  useEffect(() => {
+    const hangmanWords = [
+      "uneasy",
+      "baffle",
+      "reason",
+      "gadget",
+      "ravage",
+      "cancel",
+      "narrow",
+      "eagle",
+      "quiver",
+      "zodiac",
+      "safety",
+      "mellow",
+      "walnut",
+      "harbor",
+      "damage",
+      "impact",
+      "admire",
+      "quartz",
+      "kettle",
+      "liquid",
+      "vortex",
+      "packet",
+      "galaxy",
+      "jungle",
+      "cactus",
+      "magnet",
+      "kernel",
+      "fabric",
+      "icebox",
+      "jacket",
+      "lament",
+      "xylem",
+      "vacant",
+      "daemon",
+      "oracle",
+      "target",
+      "effort",
+      "zigzag",
+      "waffle",
+      "happen",
+      "tablet",
+      "number",
+      "salmon",
+      "paddle",
+      "banner",
+      "object",
+      "yearly",
+      "xenon",
+      "abroad",
+      "yellow",
+      "update",
+      "fallow",
+    ];
+    const selectRandomWord = () => {
+      return hangmanWords[Math.floor(Math.random() * hangmanWords.length)];
+    };
+    setRandomWord(selectRandomWord());
+  },
+    []);
+
+ 
+  return <div>
+    <p>Selected Word : {randomWord}</p>
+  </div>;
+};
+
+export default Game;
